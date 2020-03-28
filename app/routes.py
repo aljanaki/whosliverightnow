f23f831834a7d92a2fecc5e03a0985a1b5efe371
Anna <aljanaki@gmail.com>
2020-03-28 15:26:05 +0200

    basic webpage

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from datetime import date
+
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',  greeting =  "Ola")
